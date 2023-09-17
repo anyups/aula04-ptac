17e91555386998da19e101d4da3fd1c8e9e27c25
anyups <anyy.gabriele@gmail.com>
2023-09-16 23:59:32 -0400

    update style 16/09

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import handlerAcessUser from "./functions/handlerAcess"
 import { useRouter } from "next/navigation";
 import {ToastContainer, toast} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import styles from './styles.module.css';
+
 
 export default function Login() {
   const [user, setUser] = useState({
@@ -17,7 +19,7 @@ export default function Login() {
     try {
       const userAuth = await handlerAcessUser(user);
       if(userAuth.token === undefined){
-        toast.error("erro no e-mail ou senha");
+        toast.error("erro no e-mail ou senha!");
       }
       push('/pages/dashboard');
     } catch {
@@ -25,22 +27,32 @@ export default function Login() {
     }
   }
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handlerLogin}>
-        <input
-          placeholder='E-mail'
-          type="email"
-          onChange={(e) => { setUser({ ...user, email: e.target.value }) }}>
-        </input>
-        <input
-          placeholder='Senha'
-          type='password'
-          onChange={(e) => { setUser({ ...user, password: e.target.value }) }}>
-        </input>
-        <button>Entrar</button>
-      </form>
+    <div className={styles.body}>
+    <div className={styles.container}>
+	<div className={styles.screen}>
+		<div className={styles.form}>
+      <h1 className={styles.h1}>login</h1>
+			<form className={styles.login} onSubmit={handlerLogin}>
+				<div className={styles.cardInput}>
+					<input type="email" className={styles.input} placeholder="Email" onChange={(e) => { setUser({ ...user, email: e.target.value }) }}/>
+				</div>
+				<div className={styles.cardInput}>
+					<input type="password" className={styles.input} placeholder="Password" onChange={(e) => { setUser({ ...user, password: e.target.value }) }}/>
+				</div>
+				<button className={styles.botao}>
+					<span className={styles.botaotxt}>Log In</span>
+				</button>				
+			</form>
       <ToastContainer/>
-    </div>
+		</div>
+		<div className={styles.bg}>
+			<span className={styles.desenho4}></span>
+			<span className={styles.desenho3}></span>		
+			<span className={styles.desenho2}></span>
+			<span className={styles.desenho1}></span>
+		</div>		
+	</div>
+</div>
+</div>
   )
 }
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,13 +1,31 @@
+
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 import { Suspense } from "react";
 import ListUsers from "@/app/componentes/ListUsers";
+import styles from '../main.module.css';
 
 export default async function Dashboard() {
    const users = await getUsers();
     return (
         <div>
-            <Suspense fallback={<p>Carregando...</p>}>
-                 <ListUsers users={users}/>
+            <Suspense fallback={
+                <div className={styles.loading}>
+                <div className={styles.animacao}>
+                    <div className={styles.circulo}></div>
+                    <div className={styles.circulo}></div>
+                    <div className={styles.circulo}></div>
+                    <div className={styles.sombra}></div>
+                    <div className={styles.sombra}></div>
+                    <div className={styles.sombra}></div>
+                    <span>Carregando</span>
+                </div>
+                </div>
+            }>
+        <div className={styles.body}>
+            <div className={styles.card}>
+                <h1><ListUsers users={users}/></h1>
+            </div>
+        </div>
             </Suspense>
         </div>
     );
