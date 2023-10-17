ba244588e830c1b0c6835acb55dcb2dd2ac66ac2
anyups <anyy.gabriele@gmail.com>
2023-10-17 17:36:56 -0400

    update handlerAcess

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,54 +1,20 @@
 'use server'
 
-const usuario = [
-    {
-        name: "any",
-        email: "any@gmail.com",
-        password: "123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-
-    {
-        name: "iza",
-        email: "izabella@gmail.com",
-        password: "456",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "gabriel",
-        email: "gabriel@gmail.com",
-        password: "798",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "mendes",
-        email: "mendes@gmail.com",
-        password: "1011",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "kari",
-        email: "kari@gmail.com",
-        password: "1213",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    }
-]
-
-const getUserAuthenticated = (login) => {
-    let user = [];
-
-    usuario.map((usuario) =>{
-    
-    if(usuario.email === login.email && usuario.password === login.password){
-        user = usuario
-    }
-    })
-
-    return user
-   }
-
-const getUsers = () =>{
-    return usuario
-        
+const url = "https://aula-17-10-alpha.vercel.app";
+const getUserAuthenticated = async (user) => {
+    const responseOfApi =  await fetch(url + "/user/authenticated", 
+        {
+         method: "POST",
+         headers: {"Content-type":"Application/json"},
+         body: JSON.stringify(user)
+        }
+    );
+    const userAuth = await responseOfApi.json();
+    return userAuth;
 }
+
+const getUsers = async () => {
+
+}
+
 export { getUsers, getUserAuthenticated };
